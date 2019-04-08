6e0fd2fb45982c6991f7886a58cfb16cf8c4fdcc
DARA.Duong <dara.duong@PNC.LAN>
2019-04-08 11:09:58 +0700

    Form First Part

---
+++ form.php
@@ -0,0 +1,35 @@
+
+    
+    <div class="col-3">
+    <div class="card">
+        <form action="">
+            <div class="form-group">
+                <span>First Number:</span>
+                <br>
+                <input type="number" required class="form-control" >
+            </div>
+            <div class="form-group">
+                <span>Select Operator:</span>
+                <br>
+                <select name="" id="" type="number" required class="form-control">
+                    <option value="None">None</option>
+                    <option value="Add">Add</option>
+                    <option value="Minus">Minus</option>
+                    <option value="Multiply">Multiply</option>
+                    <option value="Divide">Divide</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <span>Second Number:</span>
+                <br>
+                <input type="number" required class="form-control">
+            </div>
+            <div class="form-group">
+                <button class="btn btn-success">Calculate</button>
+            </div>
+            <div class="form-group">
+                <p>The result is:0</p>
+            </div>
+        </form>
+    </div>
+    </div>
